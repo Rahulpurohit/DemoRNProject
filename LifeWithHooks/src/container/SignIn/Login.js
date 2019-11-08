1699f8444c81012972dffa03c0022d7ea47fda1e
RAHUL PUROHIT <rahulpurohit@live.com>
2019-11-09 02:17:20 +0530

    Login style Change.

--- LifeWithHooks/src/container/SignIn/Login.js
+++ LifeWithHooks/src/container/SignIn/Login.js
@@ -6,6 +6,7 @@ import {
   Text,
   TouchableOpacity,
   StatusBar,
+  ScrollView
 } from 'react-native';
 import EvilIconsIcon from 'react-native-vector-icons/EvilIcons';
 import {validations} from './index';
@@ -31,7 +32,7 @@ export default class Login extends Component {
         resolve();
         this.setState({data: values});
         this.props.getUser(values);
-      }, 500),
+      }, 500)
     );
   }
 
@@ -45,65 +46,66 @@ export default class Login extends Component {
           <ImageBackground
             style={styles.rect}
             source={require('../../assets/images/Gradient_LZGIVfZ.png')}>
-            <View style={styles.LogoColumn}>
-              <View style={styles.Logo}>
-                <View style={styles.endWrapperFiller} />
-                <View style={styles.text3Column}>
-                  <Text style={styles.text3}>Hello Hooks</Text>
-                  <View style={styles.rect7} />
-                </View>
-              </View>
-              <View style={styles.Form}>
-                <View style={styles.UsernameColumn}>
-                  <View style={styles.Username}>
-                    <EvilIconsIcon name="user" style={styles.icon2} />
-                    <TextInput
-                      placeholder="email"
-                      placeholderTextColor="rgba(255,255,255,1)"
-                      secureTextEntry={false}
-                      style={styles.UsernameInput}
-                      name="email"
-                      onChange={onChange}
-                      onValidate={onValidate}
-                      validateOn="blur"
-                      validation={validations.email}
-                      labelTitle={'Email'}
-                      placeHolder={'Email'}
-                      keyboardType={'email-address'}
-                      {...email}
-                    />
-                  </View>
-                  <View style={styles.Password}>
-                    <EvilIconsIcon name="lock" style={styles.icon2} />
-                    <TextInput
-                      placeholder="Password"
-                      placeholderTextColor="rgba(255,255,255,1)"
-                      style={styles.PasswordInput}
-                      name="password"
-                      onChange={onChange}
-                      onValidate={onValidate}
-                      validateOn="blur"
-                      validation={validations.password}
-                      labelTitle={'Password'}
-                      {...password}
-                      secureTextEntry={true}
-                      keyboardType={'email-address'}
-                    />
+            <ScrollView>
+              <View style={styles.LogoColumn}>
+                <View style={styles.Logo}>
+                  <View style={styles.endWrapperFiller} />
+                  <View style={styles.text3Column}>
+                    <Text style={styles.text3}>Hello Hooks</Text>
+                    <View style={styles.rect7} />
                   </View>
                 </View>
+                <View style={styles.Form}>
+                  <View style={styles.UsernameColumn}>
+                    <View style={styles.Username}>
+                      <EvilIconsIcon name="user" style={styles.icon2} />
+                      <TextInput
+                        placeholder="email"
+                        placeholderTextColor="rgba(255,255,255,1)"
+                        secureTextEntry={false}
+                        style={styles.UsernameInput}
+                        name="email"
+                        onChange={onChange}
+                        onValidate={onValidate}
+                        validateOn="blur"
+                        validation={validations.email}
+                        labelTitle={'Email'}
+                        placeHolder={'Email'}
+                        keyboardType={'email-address'}
+                        {...email}
+                      />
+                    </View>
+                    <View style={styles.Password}>
+                      <EvilIconsIcon name="lock" style={styles.icon2} />
+                      <TextInput
+                        placeholder="Password"
+                        placeholderTextColor="rgba(255,255,255,1)"
+                        style={styles.PasswordInput}
+                        name="password"
+                        onChange={onChange}
+                        onValidate={onValidate}
+                        validateOn="blur"
+                        validation={validations.password}
+                        labelTitle={'Password'}
+                        {...password}
+                        secureTextEntry={true}
+                        keyboardType={'email-address'}
+                      />
+                    </View>
+                  </View>
 
-                <Text style={styles.errorText}>{error}</Text>
+                  <Text style={styles.errorText}>{error}</Text>
 
-                <View style={styles.UsernameColumnFiller} />
-                <TouchableOpacity
-                  onPress={onSubmit(this.handleSubmit.bind(this))}
-                  style={styles.button}>
-                  <Text style={styles.text2}>Get Started</Text>
-                </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={onSubmit(this.handleSubmit.bind(this))}
+                    style={styles.button}>
+                    <Text style={styles.text2}>Get Started</Text>
+                  </TouchableOpacity>
+                </View>
               </View>
-            </View>
 
-            <View style={styles.LogoColumnFiller} />
+              <View style={styles.LogoColumnFiller} />
+            </ScrollView>
 
             <View style={styles.FooterTexts}>
               <TouchableOpacity
@@ -131,32 +133,32 @@ export default class Login extends Component {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    backgroundColor: 'rgb(255,255,255)',
+    backgroundColor: 'rgb(255,255,255)'
   },
   Background: {
-    flex: 1,
+    flex: 1
   },
   rect: {
     opacity: 0.69,
-    flex: 1,
+    flex: 1
   },
   Logo: {
     width: '100%',
     height: 111,
-    alignSelf: 'center',
+    alignSelf: 'center'
   },
   endWrapperFiller: {
-    flex: 1,
+    flex: 1
   },
   text3: {
     flex: 1,
     color: 'rgba(255,255,255,1)',
     fontSize: 40,
-    marginBottom: 4,
+    marginBottom: 4
   },
   rect7: {
     height: 3,
-    backgroundColor: '#25cdec',
+    backgroundColor: '#25cdec'
   },
   text3Column: {
     width: '100%',
@@ -164,31 +166,30 @@ const styles = StyleSheet.create({
 
     marginBottom: 6,
     marginLeft: 2,
-    marginRight: 3,
+    marginRight: 3
   },
   Form: {
-    height: 230,
-    marginTop: 59,
+    marginTop: 59
   },
   Username: {
     height: 70,
     backgroundColor: 'rgba(251,247,247,0.25)',
     opacity: 1,
     borderRadius: 5,
-    flexDirection: 'row',
+    flexDirection: 'row'
   },
   icon2: {
     color: 'rgba(255,255,255,1)',
     fontSize: 33,
     marginLeft: 20,
-    alignSelf: 'center',
+    alignSelf: 'center'
   },
   UsernameInput: {
     color: 'rgba(255,255,255,1)',
     flex: 1,
     marginRight: 10,
     marginLeft: 10,
-    marginTop: 4,
+    marginTop: 4
   },
   Password: {
     height: 59,
@@ -196,69 +197,70 @@ const styles = StyleSheet.create({
     opacity: 1,
     borderRadius: 5,
     flexDirection: 'row',
-    marginTop: 27,
+    marginTop: 27
   },
   PasswordInput: {
     color: 'rgba(255,255,255,1)',
     flex: 1,
     marginRight: 10,
     marginLeft: 10,
-    marginTop: 4,
+    marginTop: 4
   },
   UsernameColumn: {},
   UsernameColumnFiller: {
-    flex: 1,
+    flex: 1
   },
   button: {
-    height: 59,
+    minHeight: 59,
     backgroundColor: 'rgba(31,178,204,1)',
     borderRadius: 5,
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   errorText: {
     color: 'rgba(255,90,90,1)',
     alignSelf: 'center',
-    margin: 10,
+    margin: 10
   },
   text2: {
     color: 'rgba(255,255,255,1)',
     alignSelf: 'center',
+    justifyContent: 'center'
   },
   LogoColumn: {
     marginTop: 90,
     marginLeft: 41,
-    marginRight: 41,
+    marginRight: 41
   },
   LogoColumnFiller: {
-    flex: 1,
+    flex: 1
   },
   FooterTexts: {
     height: 14,
     flexDirection: 'row',
     marginBottom: 36,
     marginLeft: 37,
-    marginRight: 36,
+    marginRight: 36
   },
   button2: {
     width: 104,
     height: 14,
-    alignSelf: 'flex-end',
+    alignSelf: 'flex-end'
   },
   ErrorContainer: {
-    flex: 1,
+    flex: 1
   },
   CreateAccountFiller: {
-    flex: 1,
+    flex: 1
   },
   CreateAccount: {
-    color: 'rgba(255,255,255,0.5)',
+    color: 'rgba(255,255,255,0.5)'
   },
   button2Filler: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'row'
   },
   NeedHelp: {
     color: 'rgba(255,255,255,0.5)',
-    alignSelf: 'flex-end',
-  },
+    alignSelf: 'flex-end'
+  }
 });
