1ac63b804f0243c78b8233bf6c66e6b800464051
RAHUL PUROHIT <rahulpurohit@live.com>
2019-11-09 02:55:26 +0530

    Button to FC.

--- LifeWithHooks/src/component/Button/Button.js
+++ LifeWithHooks/src/component/Button/Button.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react';
-import {  Text, TouchableOpacity, ActivityIndicator } from 'react-native';
+import React, {Component} from 'react';
+import {Text, TouchableOpacity} from 'react-native';
 import getStyle from './style';
-export class Button extends Component {
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		const styles = getStyle();
-		const { running } = this.props;
-		return (
-			<TouchableOpacity onPress={ this.props.onPress} style={styles.container}>
-				{<Text style={styles.text}> {this.props.children} </Text>}
-			</TouchableOpacity>
-		);
-	}
+export function Button(props) {
+  const styles = getStyle();
+  const {running} = props;
+  return (
+    <TouchableOpacity onPress={props.onPress} style={styles.container}>
+      {<Text style={styles.text}> {props.children} </Text>}
+    </TouchableOpacity>
+  );
 }
