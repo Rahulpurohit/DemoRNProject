f9f90b695e67a09f9ab40ae3c164d51f17e61825
RAHUL PUROHIT <rahulpurohit@live.com>
2019-11-09 03:32:20 +0530

    Compontnt to hooks added.

--- LifeWithHooks/src/container/About/About.js
+++ LifeWithHooks/src/container/About/About.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import {View, Text} from 'react-native';
 
-export class About extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-
-	render() {
-		return (
-			<View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
-				<Text style={{ color: 'black' }}> About This App </Text>
-			</View>
-		);
-	}
+export function About() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text style={{color: 'black'}}> About This App </Text>
+    </View>
+  );
 }
